bccc34937131dfb861713236dedd9a63a0a07997
Jamaine Haywood <105879061+jhaywood86@users.noreply.github.com>
2022-08-11 03:28:59 -0700

    New items added

--- index.js
+++ index.js
@@ -8,15 +8,29 @@ function newImage (url, left, bottom) {
     gameObject.style.bottom = bottom + 'px'
     document.body.append(gameObject)
     return gameObject
-
 }
-newImage('assets/green-character.gif',100, 100)
-newImage('assets/pine-tree.png',450, 200)
-newImage('assets/tree.png', 200, 300)
-newImage('assets/pillar.png', 350, 100)
-newImage('assets/crate.png', 150, 200)
-newImage('assets/well.png', 500, 425)
+   newImage()
 
-    newImage()
+   function newItem(url, left, bottom){
+    let gameObject = newImage(url, left, bottom)
+
+    
+    gameObject.addEventListener('dblclick', () => {
+        gameObject.remove()
+    })
+}
+newImage('assets/green-character.gif', 100, 100);
+newImage('assets/tree.png', 200, 300);
+newImage('assets/pillar.png', 350, 100);
+newImage('assets/pine-tree.png', 450, 200);
+newImage('assets/crate.png', 150, 200);
+newImage('assets/well.png', 500, 425);
+
+newItem('assets/sword.png', 500, 405);
+newItem('assets/staff.png', 600, 100);
+newItem('assets/sheild.png', 165, 185);
+
+
+
 
 

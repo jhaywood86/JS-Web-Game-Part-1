47f9ed0caaa2aec7d433f90d28cb4c11949b1c80
Jamaine Haywood <105879061+jhaywood86@users.noreply.github.com>
2022-08-11 02:27:49 -0700

    New images added

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+
+function newImage (url, left, bottom) {
+    let gameObject = document.createElement('img')
+    gameObject.src = url
+    gameObject.style.position = 'fixed'
+    gameObject.style.left = left + 'px'
+    gameObject.style.bottom = bottom + 'px'
+    document.body.append(gameObject)
+    return gameObject
+
+}
+newImage('assets/green-character.gif',100, 100)
+newImage('assets/pine-tree.png',450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+    newImage()
+
+
